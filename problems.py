1e7fabbc52704c6b977065a843b42c9cc74b0035
adish-ct <adishathu525@gmail.com>
2023-12-09 09:44:12 +0530

    divmod

---
+++ problems.py
@@ -0,0 +1,15 @@
+# 1.    Finding root of a number
+x = 25
+print(x ** 0.5)
+
+# 2.    Reversing string without using inbuilt function
+
+string = "hello"
+new_string = ""
+
+for i in string:
+    new_string = i + new_string
+
+print(new_string)
+
+
