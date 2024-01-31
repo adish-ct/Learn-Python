16345d0adba2373de51451fe1afef657b2c6bae6
adish-ct <adishathu525@gmail.com>
2024-01-31 21:07:46 +0530

    Feat(string): add string function

--- strings.py
+++ strings.py
@@ -13,3 +13,20 @@ else:
 #  Using the f'' string
     
 print(f'{'a is greater than 5' if a > 5 else 'a is less than 5'}')
+
+
+"""     Swap case  strings """
+
+"""
+It is a string’s function that converts all uppercase characters into 
+lowercase and vice versa. It is used to alter the existing case of the string. 
+This method creates a copy of the string which contains all the characters in the swap case
+
+"""
+
+string = "Hello World"
+
+swapped_string = string.swapcase()
+
+print(swapped_string)
+# output : hELLO wORLD
